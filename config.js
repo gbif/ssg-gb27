ce245a63d38598029bb01192d289f230af141f0a
Matthew Blissett <mblissett@gbif.org>
2023-04-24 12:21:07 +0200

    Remove Google Analytics

--- config.js
+++ config.js
@@ -17,6 +17,5 @@ module.exports = {
     fonts: {
         iconsSrc: ['./bower_components/gbif-ssg/bower_build/icons/**/*.svg', src + '/icons/**/*.*'],
         template: './bower_components/gbif-ssg/bower_build/icons/fonttemplate.styl'
-    },
-    googleAnalyticsId: 'UA-42057855-12'
+    }
 };
